170b0cf7d7c912cf1b068499d3d77bf6ec2827d7
Esteban MESTRIE <e.mestrie@isartdigital.com>
2019-11-09 15:49:59 +0100

    export de header

--- App.js
+++ App.js
@@ -1,45 +1,11 @@
 import React from 'react';
-import {  
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-  TextInput ,
-  html,
-}from 'react-native';
+import { } from 'react-native';
+import Header from './components/header';
 
 
 export default function App() {
   
   return (
-    <View style={styles.container}>
-      <Text style={styles.hello2}>Hello, world</Text>
-      <Text>Hello, world</Text>
-      <Button
-          title="Press me"
-          onPress={() => Alert.alert(inputValue)}
-        />
-        
-        <TextInput
-      onChange={ this.handleChange }
-      style={{ width: 200, height:20, borderColor: 'white', borderWidth: 1 }}
-    />
-    
-    </View>
+    <Header />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3E53C8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  hello2:{  
-    fontSize: 20, 
-    color:'#DCDEE9',
-  },
-});
---
+++ components/header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+     <Text>Liste de courses</Text>
+);
+
+export default Header;
